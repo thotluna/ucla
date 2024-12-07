cdf0ea52710fbb413f7edb65c72288b993dfd1dc
Eladio Feijoo <eladio.feijoo@gmal.com>
2024-12-07 17:26:33 -0400

    rebase

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless'
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  site: 'https://ucla-xi.vercel.app/',
+  integrations: [tailwind(), sitemap()],
+  output: 'server',
+  adapter: vercel({
+    imageService: false
+  })
+
 });
