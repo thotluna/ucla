08d1a719fc942c25d6be646286858cb39ff89ef4
Eladio Feijoo <eladio.feijoo@gmal.com>
2024-12-07 16:53:10 -0400

    add new ssr config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ucla-xi.vercel.app/',
-  output: 'server',
   integrations: [tailwind(), sitemap()],
-  adapter: vercel({})
+  output: 'server',
+  adapter: vercel({
+    imageService: false
+  })
+
 });
