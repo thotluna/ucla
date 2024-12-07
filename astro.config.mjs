9b70533fb2641998b05698a1de84936565d1b26f
Eladio Feijoo <eladio.feijoo@gmal.com>
2024-12-07 17:56:44 -0400

    fix ssr remove

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless'
+
 
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ucla-xi.vercel.app/',
   integrations: [tailwind(), sitemap()],
-  output: 'server',
-  adapter: vercel({
-    imageService: false
-  })
 
 });
