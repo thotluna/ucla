722c27d40c0e72e6f0210ecd1088656d2576c0ff
Eladio Feijoo <eladio.feijoo@gmal.com>
2024-12-08 18:49:59 -0400

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,25 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
+// import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ucla-xi.vercel.app/',
   integrations: [tailwind(), sitemap()],
-  output: "server",
-  adapter: vercel({
-		webAnalytics: {
-			enabled: true,
-		},
-	}),
-  vite:{
-    build: {
-			cssMinify: "lightningcss",
-		},
-    ssr:{
-      external:["path-to-regexp"]
-    }
-  }
+  // output: "server",
+  // adapter: vercel({
+	// 	webAnalytics: {
+	// 		enabled: true,
+	// 	},
+	// }),
+  // vite:{
+  //   build: {
+	// 		cssMinify: "lightningcss",
+	// 	},
+  //   ssr:{
+  //     external:["path-to-regexp"]
+  //   }
+  // }
 
 });
